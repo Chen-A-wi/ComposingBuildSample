24d78a51262ed8d9116b2b425705da857d3135b9
Ben <tttw216@gmail.com>
2023-06-05 14:53:35 +0800

    [Fix] remove compose extension

--- plugins/src/main/java/extension/CommonExt.kt
+++ plugins/src/main/java/extension/CommonExt.kt
@@ -1,28 +1,8 @@
 package extension
 
 import com.android.build.api.dsl.CommonExtension
-import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.plugins.ExtensionAware
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
-import plugins.Version
-import plugins.composeVersion
-
-fun CommonExtension<*, *, *, *>.composeConfig(libs: VersionCatalog) {
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.composeVersion
-    }
-
-    packaging {
-        resources.excludes.apply {
-            add("META-INF/AL2.0")
-            add("META-INF/LGPL2.1")
-        }
-    }
-}
 
 fun CommonExtension<*, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
     (this as ExtensionAware).extensions.configure("kotlinOptions", block)
