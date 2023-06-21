77ee35b6c319b7cca0b1bff5e1f9378ffce82b26
BenChen <tttw216@gmail.com>
2023-06-21 09:59:39 +0800

    [Feat] move compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,6 @@ android {
 
 dependencies {
     implementation(libs.bundles.androidx)
-    implementation(libs.bundles.compose)
     testImplementation(libs.test.junit)
     androidTestImplementation(libs.bundles.androidx.test)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.compose.ui.test.junit4)
-    debugImplementation(libs.compose.ui.tooling)
-    debugImplementation(libs.compose.ui.test.manifest)
 }
--- plugins/src/main/java/plugins/ComposePlugin.kt
+++ plugins/src/main/java/plugins/ComposePlugin.kt
@@ -4,6 +4,8 @@ import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.the
 
 class ComposePlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -15,6 +17,16 @@ class ComposePlugin : Plugin<Project> {
 
                 composeOptions {
                     kotlinCompilerExtensionVersion = Version.composeCompiler
+                }
+
+                val libs = the<org.gradle.accessors.dm.LibrariesForLibs>()
+
+                dependencies {
+                    "implementation"(libs.bundles.compose)
+                    "androidTestImplementation"(libs.compose.ui.test.junit4)
+                    "androidTestImplementation"(platform(libs.compose.bom))
+                    "debugImplementation"(libs.compose.ui.tooling)
+                    "debugImplementation"(libs.compose.ui.test.manifest)
                 }
             }
         }
